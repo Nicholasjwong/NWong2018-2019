27093084d27a37965771cfa5e760c182fdc73fb0
APCS3rd <APCS3rd@MHS-Math2-26>
2018-08-20 10:45:22 -0700

    first commit to github

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,17 @@
+
+/**
+ * Prints message in console
+ * 
+ * @author Nicholas Wong 
+ * @version 8/17/2018
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Nicholas Wong"); 
+        System.out.println("Goodbye World! From Not Nicholas Wong");
+        System.out.println("Actually I am back because I am sitting here bored in class");
+    }
+}
+
